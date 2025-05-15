18943382049ac0bc4971d39f9e6a8cf067509891
MarioKoko777 <lilcocomusicoficial@gmail.com>
2025-05-15 12:55:37 +0200

    Update Asignatura.js

--- Task 4.3/js/Asignatura.js
+++ Task 4.3/js/Asignatura.js
@@ -1,7 +1,19 @@
+/**
+ * Representa una asignatura en el sistema académico.
+ */
 export class Asignatura {
+  /**
+   * Crea una nueva instancia de Asignatura.
+   * @param {number} id - Identificador único de la asignatura.
+   * @param {string} nombre - Nombre de la asignatura.
+   * @param {string} curso - Curso o nivel educativo al que pertenece.
+   */
   constructor(id, nombre, curso) {
+    /** @type {number} */
     this.id = id;
+    /** @type {string} */
     this.nombre = nombre;
+    /** @type {string} */
     this.curso = curso;
   }
 }
